9a2622008b124a521305b8aa30ce6cb9be5170d8
Collette Harrison <coamharr@ucsc.edu>
2023-10-09 14:11:49 -0700

    timer

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let countdown = 10;
+
+
 class SceneA extends Phaser.Scene {
   constructor() {
     super({ key: 'SceneA' });
@@ -8,14 +11,24 @@ class SceneA extends Phaser.Scene {
                 this.input.on('pointerdown', () => this.scene.start('intro'));
 
 }
+update(){
+  {countdown--;}
+  console.log('A', countdown)
 }
+}
+
+
+
 class Intro extends Phaser.Scene {
   constructor() {
       super('intro');}
       create(){
         this.add.text(200,100,"Click to begin", {fontSize: 65, fill: 'red', fontStyle: 'italic'});
                     this.input.on('pointerdown', () => this.scene.start('SceneA'));}
-
+update(){
+  {countdown--;}
+  console.log('B', countdown)
+}
 }
 
 
