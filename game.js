539072958b186dffb6b05bb94fcf5bfa3ca739a5
Collette Harrison <coamharr@ucsc.edu>
2023-10-10 16:39:57 -0700

    store burger ingredient and compare to order

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ let countdown = 10;
 let is_paper = 0;
 let is_cheese = 0;
 let is_lettuce = 0;
+let burger = {};
 
 class SceneA extends Phaser.Scene {
   constructor() {
@@ -55,8 +56,22 @@ preload(){
     
 }
 create(){
+
   this.graphics = this.add.graphics();
+  //the image objects and the number of times they have been clicked
   this.imageObjects = [];
+  //current burger elements
+  this.burger = {};
+  this.burger['cheese'] = 0;
+  this.burger['lettuce'] = 0;
+  //contians the ingrediatns to the burger
+  const order1 = {'cheese': Phaser.Math.Between(0, 2), 'lettuce': Phaser.Math.Between(0, 3)}
+  console.log('order', order1);
+  //randomized order
+  //probably declare later
+  const keyboard = this.input.keyboard;
+  // Create a keyboard input manager
+  
   this.imageObject = this.add.sprite(
     380,
     380,
@@ -72,7 +87,6 @@ this.imageObject.setScale(.05);
 arrow.setInteractive()
 arrow.on('pointerdown', ()=> {
   this.scene.start('SceneA')}
-
 )
 
 let paper = this.imageObject = this.add.image(300, 540,'cheese').setInteractive()
@@ -85,19 +99,60 @@ let Cheese = this.imageObject = this.add.image(600,430,'cheese').setInteractive(
 Cheese.setScale(.2);
 Cheese.on('pointerdown', ()=> {
   this.createImage(440, 430, 'cheese', .2)
-  is_cheese ++
-  //console.log('create cheese', is_cheese)
+  //console.log('cheese count in create', is_cheese)
 })
 
 let Lettuce = this.add.image (700, 430, 'lettuce').setInteractive()
 Lettuce.setScale(.2);
 Lettuce.on('pointerdown', ()=>{
   this.createImage(450, 430, 'lettuce', 0.4);
-  is_lettuce ++
+})
+keyboard.on('keydown-SPACE', () => {
+  console.log(this.burger);
+  const objectsMatch = (obj1, obj2) => {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+    if (keys1.length !== keys2.length) {
+      return false; // Objects have different numbers of keys
+    }
+    for (const key of keys1) {
+      if (obj1[key] !== obj2[key]) {
+        return false; // Values for the same key are different
+      }
+    }
+    return true;
+  };
+  if (objectsMatch(this.burger, order1)) {
+    console.log('The objects match.');
+  } else {
+    console.log('The objects do not match.');
+  }
+
+
 })
 }
+
+
+
+
+
 createImage(x, y, pic, z) {
+  const subtractValueByKey = (object, key) => {
+    if (object[key] !== undefined) {
+      if (object[key] >= 1){
+        object[key] -= 1;
+        
+      }
+    }}
+
   this.pic = pic
+  if (pic == 'cheese'){
+    is_cheese ++
+    this.burger['cheese'] = is_cheese;
+  }else if (pic == 'lettuce'){
+    is_lettuce ++
+    this.burger['lettuce'] = is_lettuce
+  }
   // Create an image object
   const image = this.add.image(x, y - number, pic);
   image.setScale(z)
@@ -105,7 +160,6 @@ createImage(x, y, pic, z) {
   // Enable input on the image for click events
   image.setInteractive();
   number ++
-  //console.log(number)
 
   // Initialize a click count for the image
   image.clickCount = 0;
@@ -113,20 +167,33 @@ createImage(x, y, pic, z) {
   // Set a click event handler for the image
   image.on('pointerdown', () => {
     image.clickCount += 1;
-
-    if (image.clickCount >= 2) {
+    if (image.clickCount >= 1) {
       image.x = 700
       image.y = 550
       //console.log('Image has been clicked more than once.');
       // You can perform additional actions for multiple clicks here
       if(pic == 'cheese'){
+        const keyToSearch = 'cheese'; // Change this to the key you want to search
+        subtractValueByKey(burger, keyToSearch);
+        if (is_cheese >= 1){
         is_cheese --
+        this.burger['cheese'] = is_cheese;
+        }
+        
         //console.log('trash',is_cheese)
       } else if (pic == 'lettuce'){
-        is_lettuce --
+        const keyToSearch = 'lettuce';
+        subtractValueByKey(burger, keyToSearch);
+        if(is_lettuce >= 1){
+          is_lettuce --
+          this.burger['lettuce'] = is_lettuce;
+        }
         //console.log('lettuce',is_lettuce)
       }
-    }})
+      console.log(this.burger)
+    }
+  
+  })
     this.imageObjects.push(image);
   }
 
@@ -147,3 +214,12 @@ let config = {
 }
 
 let game = new Phaser.Game(config);
+
+
+/*let text = this.add.text(200,100,"Gross, don't touch that!", {fontSize: 65, fill: '#fff2cc', fontStyle: 'italic'});
+        this.tweens.add({
+          targets: text,
+          alpha: 0, // Target alpha value (fully transparent)
+          duration: 1000, // Duration of the tween in milliseconds
+          ease: 'Linear',})
+          console.log(object[key])*/
